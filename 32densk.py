b3f19c7dd0d363867c21a7056c02ec85a9ec1460
elmerhomero53 <53914081+elmerhomero53@users.noreply.github.com>
2020-06-07 16:56:01 -0600

    ya casi

--- 32densk.py
+++ 32densk.py
@@ -18,7 +18,6 @@ import numpy as np
 
 from mpl_toolkits.mplot3d import Axes3D 
 import matplotlib.pyplot as plt
-from numpy.polynomial.legendre import Legendre as leg
 from matplotlib import cm
 
 fig = plt.figure()
@@ -30,45 +29,47 @@ warm = cm.coolwarm
 """La pregunta 3.2
 """
 
-sin = np.sin
 pi = np.pi
-
-def h(y):
-    return 2*y**3+5
-
-cosh = np.cosh
-sh = np.shape
+dr = 0.5
 cos = np.cos
-
-dr = 0.1
+sh = np.shape
 
 R = 3
-r = np.arange(0,R*3,dr)
-t = np.linspace(0,pi,len(r))
+b = 3*R
 
+r = np.arange(0,b,dr)
 dt = pi/len(r)
+Ri = len(r[:int(len(r)/3)])
 
+r1 = np.arange(b,b*1.5,dr)
+r = np.concatenate((r,r1))
+
+t = np.linspace(0,pi,len(r))
 X,Y = np.meshgrid(r,t)
 
-a1 = 3/10
-a3 = -12/(35*R**2)
-b1 = a1*R**3
-b3 = a3*R**7
+a0 = -b/3
+a1 = R*b/(3)
+a2 = 4/(b*15)
+a3 = -a2*R**5
     
 def p3(x):
-    y = 1/2*(5*cos(x)**3-3*cos(x))
+    y = 1/2*(3*cos(x)**2-cos(x))
     return y
 
-def V(r,t):
+def V(r,t,Ri):
     v = np.zeros((len(r),len(t)))
-    n = int(len(r))
-    k = 0
+    k = Ri
     p = 0 
-    Ri = len(r[:int(len(r)/2)])
-    for rad in r[Ri:]:
+    for rad in r[Ri:Ri*3]:
         p = 0
         for th in t:
-            v[k][p] = b1*cos(th)/(rad**2)+b3/(rad**4)*p3(th)
+            v[p][k] = a0 + a1/(3*rad) + (a2*rad**2+a3/rad**3)*p3(th)
+            p += 1
+        k+=1
+    for rad in r[Ri*3:]:
+        p =0 
+        for th in t:
+            v[p][k] = a0 + a1/(3*rad) + (a2*rad**2+a3/rad**3)*p3(th)
             p += 1
         k+=1
     return v
@@ -76,12 +77,12 @@ def V(r,t):
 #x,y = np.meshgrid(x,y)
 
 
-def graficarV():
-    v = V(r,t)
+def graficarV(Ri):
+    v = V(r,t, Ri)
     ax = fig.gca(projection='3d')
     surf = ax.plot_surface(X,Y,v, cmap=cm.coolwarm,
                            linewidth=0, antialiased=False)
-    ax.set_ylabel('Theta (radianes)')
+    ax.set_ylabel(r'$\theta$ (radianes)')
     ax.set_xlabel('R (m)')
     ax.set_title('Potencial (V)')
     fig.colorbar(surf, shrink=0.5, aspect=5)
@@ -91,20 +92,19 @@ def menu():
     print('quiere graficar: \n'
           '1. el campo electrico\n'
           '2. el potencial\n'
-          '3. la carga superficial\n'
           'ingrese el numero de la opcion que desea por favor')
     ans = input('')
     if '2' in ans:
-        v = graficarV()
+        v = graficarV(Ri)
     elif '1' in ans:
         theta = np.linspace(0,1,3)
-        v = V(r,t)
+        v = V(r,t, Ri)
         ex, ey = np.gradient(v, dr, dt)
         fig, ax = plt.subplots()
         ax.set_title('Campo eléctrico (V/m)')
         ax.set_ylabel(r'$\theta$ (radianes)')
         ax.set_xlabel('R (m)')
-        Q = ax.quiver(X,Y, ex, ey, linewidth = 0.01)
+        Q = ax.quiver(X,Y, ex, ey, linewidth = 0.15)
         plt.show()
         
 menu()
